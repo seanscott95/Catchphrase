7d6d021931b2ef229093988860476bdfb9e39502
Sean <seanms418@gmail.com>
2023-05-12 18:34:13 +1000

    initial commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <h1 className="page-title">CATCH PHRASE</h1>
+      <button className="play-btn">PLAY</button>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Homepage from "../components/Homepage";
+
+const MainLayout = () => {
+  return (
+    <>
+        <Homepage />
+    </>
+  );
+};
+
+export default MainLayout;
