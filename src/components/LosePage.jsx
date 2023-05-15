fa1e637eb513a092e32a7ca9fc1a602c08eea087
Sean <seanms418@gmail.com>
2023-05-15 21:31:37 +1000

    added the lose page component to the main layout component and styled it

--- src/components/LosePage.jsx
+++ src/components/LosePage.jsx
@@ -1,17 +1,17 @@
 
-const LosePage = ({handleHomeBtnClick, handleStartGame}) => {
-  return (
-    <div className='loserPageContainer'>
-        <section>
-            <h1>YOU LOSE</h1>
-            <h2>PHRASES: </h2>
-        </section>
-        <section className='footer'>
+const LosePage = ({ count, handleHomeBtnClick, handleStartGame }) => {
+    return (
+        <div className='loserPageContainer'>
+            <section>
+                <h1>YOU LOSE</h1>
+                <h2>PHRASES: {count}</h2>
+            </section>
+            <section className='footer'>
                 <button onClick={handleHomeBtnClick} className='navbarBtn'>HOME</button>
                 <button onClick={handleStartGame} className='navbarBtn'>PLAY</button>
-        </section>
-    </div>
-  );
+            </section>
+        </div>
+    );
 };
 
 export default LosePage;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,94 +2,107 @@ import { useState } from "react";
 import HomepageComp from "../components/Homepage";
 import CategoriesPageComp from "../components/CategoriesPage";
 import RulesPageComp from "../components/RulesPage";
+import LosePageComp from "../components/LosePage";
 
 const MainLayout = () => {
-    const [homepage, setHomepage] = useState(true);
-    const [categoriesPage, setCategoriesPage] = useState(false);
-    const [rulesPage, setRulesPage] = useState(false);
+  const [homepage, setHomepage] = useState(true);
+  const [categoriesPage, setCategoriesPage] = useState(false);
+  const [rulesPage, setRulesPage] = useState(false);
+  const [losePage, setLosePage] = useState(false);
 
-    const [categories, setCategories] = useState([
-      {
-          name: 'PARTY',
-          include: true,
-      },
-      {
-          name: 'POP',
-          include: true,
-      },
-      {
-          name: 'PEOPLE',
-          include: true,
-      },
-      {
-          name: 'TV',
-          include: true,
-      },
-      {
-          name: 'WORLD',
-          include: true,
-      },
-      {
-          name: 'SLANG',
-          include: true,
-      },
-      {
-          name: 'BRANDS',
-          include: true,
-      },
-      {
-          name: 'NERD',
-          include: true,
-      },
-      {
-          name: 'SPORTS',
-          include: true,
-      },
-      {
-          name: 'MUSIC',
-          include: true,
-      },
-      {
-          name: 'GAMES',
-          include: true,
-      },
+  const [categories, setCategories] = useState([
+    {
+      name: 'PARTY',
+      include: true,
+    },
+    {
+      name: 'POP',
+      include: true,
+    },
+    {
+      name: 'PEOPLE',
+      include: true,
+    },
+    {
+      name: 'TV',
+      include: true,
+    },
+    {
+      name: 'WORLD',
+      include: true,
+    },
+    {
+      name: 'SLANG',
+      include: true,
+    },
+    {
+      name: 'BRANDS',
+      include: true,
+    },
+    {
+      name: 'NERD',
+      include: true,
+    },
+    {
+      name: 'SPORTS',
+      include: true,
+    },
+    {
+      name: 'MUSIC',
+      include: true,
+    },
+    {
+      name: 'GAMES',
+      include: true,
+    },
   ]);
 
-    const handleHomeBtnClick = () => {
-      setHomepage(true);
-      setCategoriesPage(false);
-    };
+  const handleHomeBtnClick = () => {
+    setHomepage(true);
+    setCategoriesPage(false);
+    setRulesPage(false);
+    setLosePage(false);
+  };
 
-    const handleNextBtnClick = () => {
-        setHomepage(false);
-        setCategoriesPage(false);
-        setRulesPage(true);
-    };
+  const handleNextBtnClick = () => {
+    setHomepage(false);
+    setCategoriesPage(false);
+    setRulesPage(true);
+    setLosePage(false);
+  };
 
-    const handleBackBtnClick = () => {
-        setHomepage(false);
-        setCategoriesPage(true);
-        setRulesPage(false);
-    };
+  const handleBackBtnClick = () => {
+    setHomepage(false);
+    setCategoriesPage(true);
+    setRulesPage(false);
+    setLosePage(false);
+  };
 
   return (
     <>
-        {homepage ?
+      {homepage ?
         <HomepageComp
           setHomepage={setHomepage}
           setCategoriesPage={setCategoriesPage}
         /> : <></>}
-        {categoriesPage ?
-          <CategoriesPageComp
-            categories={categories}
-            setCategories={setCategories}
-            handleHomeBtnClick={handleHomeBtnClick}
-            handleNextBtnClick={handleNextBtnClick}
-          /> : <></>}
-        {rulesPage ?
+      {categoriesPage ?
+        <CategoriesPageComp
+          categories={categories}
+          setCategories={setCategories}
+          handleHomeBtnClick={handleHomeBtnClick}
+          handleNextBtnClick={handleNextBtnClick}
+        /> : <></>}
+      {rulesPage ?
         <RulesPageComp
           handleBackBtnClick={handleBackBtnClick}
         /> : <></>}
+      {losePage ?
+        <LosePageComp
+          count={count}
+          handleHomeBtnClick={handleHomeBtnClick}
+          handleStartGame={handleStartGame}
+        /> : <></>
+      }
     </>
   );
 };
