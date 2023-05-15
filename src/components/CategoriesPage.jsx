06c3685cbc4a2e7c63975f94d6c0b606cd38edec
Sean <seanms418@gmail.com>
2023-05-15 19:55:47 +1000

    when category button is clicked the categories state for that category will change to have the property include set to thge opposite, any property that has unclude falsy will have a red line through the text and background color will change

--- src/components/CategoriesPage.jsx
+++ src/components/CategoriesPage.jsx
@@ -1,20 +1,67 @@
+import { useState } from "react";
 
 const CategoriesPage = ({ handleHomeBtnClick }) => {
 
-    const categories = [
-        'PARTY',
-        'POP',
-        'PEOPLE',
-        'TV',
-        'WORLD',
-        'SLANG',
-        'BRANDS',
-        'NERD',
-        'SPORTS',
-        'MUSIC',
-        'GAMES',
-    ];
+    const handleCategoryBtn = (e) => {
+        const category = e.target.value;
+        const updatedArray = categories.map(obj => {
+            if (obj.name === category) {
+                obj.include = !obj.include;
+            };
+            return obj;
+        });
+        setCategories(updatedArray);
+    };
+    
+    const [categories, setCategories] = useState([
+        {
+            name: 'PARTY',
+            include: true,
+        },
+        {
+            name: 'POP',
+            include: true,
+        },
+        {
+            name: 'PEOPLE',
+            include: true,
+        },
+        {
+            name: 'TV',
+            include: true,
+        },
+        {
+            name: 'WORLD',
+            include: true,
+        },
+        {
+            name: 'SLANG',
+            include: true,
+        },
+        {
+            name: 'BRANDS',
+            include: true,
+        },
+        {
+            name: 'NERD',
+            include: true,
+        },
+        {
+            name: 'SPORTS',
+            include: true,
+        },
+        {
+            name: 'MUSIC',
+            include: true,
+        },
+        {
+            name: 'GAMES',
+            include: true,
+        },
+    ]);
 
+    console.log(categories)
+    
     return (
         <div className='categoryPageContainer'>
             <section className='navbar'>
@@ -25,7 +72,22 @@ const CategoriesPage = ({ handleHomeBtnClick }) => {
 
             <section className='categories'>
                 {categories.map(item => (
-                    <button className="categoryBtn" key={item}>{item}</button>
+                    <>
+                        {item.include ?
+                            <button
+                                className="categoryBtn"
+                                key={item.name}
+                                onClick={handleCategoryBtn}
+                                value={item.name}
+                            >{item.name}</button>
+                            : <button
+                                className="categoryBtnUnclicked"
+                                key={item.name}
+                                onClick={handleCategoryBtn}
+                                value={item.name}
+                            >{item.name}</button>
+                        }
+                    </>
                 ))}
             </section>
 
