449401fe7749dca239879e4a79dde3ee93a3c581
Sean <seanms418@gmail.com>
2023-05-15 20:52:06 +1000

    created rules page that displays the instructions

--- src/components/RulesPage.jsx
+++ src/components/RulesPage.jsx
@@ -1,7 +1,26 @@
-const RulesPage = () => {
-  return (
-    <div>RulesPage</div>
-  );
+const RulesPage = ({ handleBackBtnClick, handleStartGame }) => {
+    return (
+        <div className='rulesPageContainer'>
+            <section className='navbar'>
+                <button onClick={handleBackBtnClick} className='navbarBtn'>BACK</button>
+                <h1 className='pageTitle'>RULES</h1>
+                <button onClick={handleStartGame} className='navbarBtn'>PLAY</button>
+            </section>
+
+            <section className='rules'>
+                <button>HOT POTATO</button>
+                <div className='instructions'>
+                    <h1>HOT POTATO</h1>
+                    <ul>
+                        <li>USE HINTS TO GET OTHERS TO GUESS THE PHRASE</li>
+                        <li>YOU CANNOT USE ANY WORD IN THE PHRASE ITSELF</li>
+                        <li>PASS THE PHONE TO THE NEXT PERSON AFTER EACH PHRASE</li>
+                        <li>THE LOSER IS THE ONE HOLDING THE PHONE WHEN THE TIMER ENDS</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default RulesPage;
