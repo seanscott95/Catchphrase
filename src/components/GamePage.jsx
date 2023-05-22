f95ee7e5d4a9a2dc1969f944f0a19cbd7932f724
Sean <seanms418@gmail.com>
2023-05-22 14:45:25 +1000

    changed the onClick home btn function to the homepageBtnClick function coming from mainlayout that resets the rules page to default settings, created another timer for the streak rules, when timerStreak is activated it will start the timer for 12 seconds, when the correct button is clicked the timer will reset the 12 seconds

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,60 +1,88 @@
 import { useState, useEffect } from "react";
 
-const GamePage = ({ setCount, currentList, handlePageChange, timer, setTimer, rules }) => {
-    const [currentWord, setCurrentWord] = useState(currentList[0] || '');
-    const [disabled, setDisabled] = useState('');
+const GamePage =
+    ({
+        setCount,
+        currentList,
+        handlePageChange,
+        timer,
+        rules,
+        homepageBtnClick,
+        timerStreak,
+        setTimerStreak,
+    }) => {
+        const [currentWord, setCurrentWord] = useState(currentList[0] || '');
+        const [disabled, setDisabled] = useState('');
 
-    // Starts quiz for 60 seconds when timer is truthy
-    useEffect(() => {
-        let timeId;
-        
-        if (timer) {
-            if (rules === 'HOT POTATO') {
-                timeId = setTimeout(() => handlePageChange('losePage'), 60000);
-            } else if (rules === 'HIGHSCORE') {
-                timeId = setTimeout(() => handlePageChange('winnerPage'), 60000);
-            } else {
-                timeId = setTimeout(() => handlePageChange('winnerPage'), 12000);
+        const handleGameButtons = () => {
+            // Disables buttons for two seconds
+            setDisabled(true);
+            setTimeout(() => {
+                setDisabled('');
+            }, 2000);
+
+            // Renders the next word in the list
+            const index = currentList.indexOf(currentWord);
+            setCurrentWord(currentList[index + 1]);
+        };
+
+        const handleCorrectBtn = () => {
+            handleGameButtons();
+            setCount(count => count + 1);
+            // Resets the timer during streak rules
+            if (rules === 'STREAK') {
+                setTimerStreak(false);
             };
         };
-        return () => {
-            clearTimeout(timeId);
-        };
-    }, [timer]);
 
-    const handleGameButtons = () => {
-        // Disables buttons for two seconds
-        setDisabled(true);
-        setTimeout(() => {
-            setDisabled('');
-        }, 2000);
+        // Starts quiz for 60 seconds when timer is truthy
+        useEffect(() => {
+            let timeId;
 
-        // Renders the next word in the list
-        const index = currentList.indexOf(currentWord);
-        setCurrentWord(currentList[index + 1]);
+            if (timer) {
+                if (rules === 'HOT POTATO') {
+                    timeId = setTimeout(() => handlePageChange('losePage'), 60000);
+                };
+                if (rules === 'HIGHSCORE') {
+                    timeId = setTimeout(() => handlePageChange('winnerPage'), 60000);
+                };
+            };
+            return () => {
+                clearTimeout(timeId);
+            };
+        }, [timer]);
+
+        // Starts the streak quiz for 12 seconds when timerStreak is truthy
+        useEffect(() => {
+            let timeIdStreak;
+
+            // Starts timer
+            if (timerStreak) {
+                timeIdStreak = setTimeout(() => handlePageChange('winnerPage'), 12000);
+            };
+            // Resets the timer if timerStreak is falsy
+            if(!timerStreak) {
+                setTimerStreak(true);
+            };
+            return () => {
+                clearTimeout(timeIdStreak);
+            };
+        }, [timerStreak]);
+
+        return (
+            <div className='gamePageContainer'>
+                <section className='navbar'>
+                    <button onClick={homepageBtnClick} className='navbarBtn'>HOME</button>
+                </section>
+                <section className="currentWord">
+                    <h1>{currentWord}</h1>
+                </section>
+                <section className='footer'>
+                    <button onClick={handleGameButtons} disabled={disabled} className='navbarBtn'>SKIP</button>
+                    <button onClick={handleCorrectBtn} disabled={disabled} className='navbarBtn'>CORRECT</button>
+                </section>
+            </div>
+        );
     };
-
-    const handleCorrectBtn = () => {
-        handleGameButtons();
-        setTimer(false);
-        setCount(count => count + 1);
-        setTimer(true);
-    };
-
-    return (
-        <div className='gamePageContainer'>
-            <section className='navbar'>
-                <button onClick={() => handlePageChange('homepage')} className='navbarBtn'>HOME</button>
-            </section>
-            <section className="currentWord">
-                <h1>{currentWord}</h1>
-            </section>
-            <section className='footer'>
-                <button onClick={handleGameButtons} disabled={disabled} className='navbarBtn'>SKIP</button>
-                <button onClick={handleCorrectBtn} disabled={disabled} className='navbarBtn'>CORRECT</button>
-            </section>
-        </div>
-    );
-};
 
 export default GamePage;
